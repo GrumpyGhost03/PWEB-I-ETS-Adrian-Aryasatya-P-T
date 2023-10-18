28f477a781f54257a25e83e04ddd563b7d41c615
GrumpyGhost03 <adrian.tjindarbumi@gmail.com>
2023-10-18 19:28:39 +0700

    feat: ETS PWEB Adrian

--- ETS Pweb/script.js
+++ ETS Pweb/script.js
@@ -1,21 +1,20 @@
-const url = 'http://it-its.id/api/movies';
-const options = {
-	method: 'GET',
-};
+// URL of the API
+const apiUrl = 'https://it-its.id/api/movies';
 
-async function GETdata(){
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        const data = result.description;
-        console.log(data);
-        data.forEach(item => {
-            const output = `<div> ${item.TITLE} </div>`;
-            document.querySelector('.movies').innerHTML+= output;
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-GETdata();
+// Fetch the API data
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Process the data
+    data.forEach(movie => {
+      console.log('Title:', movie.Title);
+      console.log('Director:', movie.Director);
+      console.log('Genre:', movie.Genre);
+      console.log('Plot:', movie.Plot);
+      console.log('Poster:', movie.Poster);
+      console.log('----------------------');
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });

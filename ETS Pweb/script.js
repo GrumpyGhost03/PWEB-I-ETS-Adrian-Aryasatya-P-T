80f81d7b679e71fcef7a870766f591ee012192bf
GrumpyGhost03 <adrian.tjindarbumi@gmail.com>
2023-10-24 21:14:02 +0700

    feat: Perbaikan PWEB Adrian

--- ETS Pweb/script.js
+++ ETS Pweb/script.js
@@ -1,20 +1,34 @@
-// URL of the API
-const apiUrl = 'https://it-its.id/api/movies';
+$(document).ready(function () {
 
-// Fetch the API data
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Process the data
-    data.forEach(movie => {
-      console.log('Title:', movie.Title);
-      console.log('Director:', movie.Director);
-      console.log('Genre:', movie.Genre);
-      console.log('Plot:', movie.Plot);
-      console.log('Poster:', movie.Poster);
-      console.log('----------------------');
+    $.ajax({
+        url: 'https://it-its.id/api/movies',
+        method: 'GET',
+        success: function (data) {
+            renderMovies(data);
+        },
+        error: function (error) {
+            console.error("An error occurred while fetching data: ", error);
+        }
     });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
+  
+    // Function to display movies
+    function renderMovies(movies) {
+        let movieList = '';
+  
+        $.each(movies, function (key, movie) {
+            movieList += `
+            <div class="col-lg-6 col-12 mb-4">
+                <div class="card shadow p-3 bg-white rounded">
+                    <div class="card-body ">
+                        <h5 class="card-title">${movie.Title}</h5>
+                        <p class="card-text">${movie.Plot}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+  
+        $('#moviesList').html(movieList);
+    }
   });
+  
